6becf51ae99b5adbd0db92fa79ed4ae2b66e191d
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-23 23:11:58 +0000

    ISS Spotter With Promises

---
+++ index2.js
@@ -0,0 +1,33 @@
+// const { fetchMyIP } = require('./iss_promised');
+// const { fetchCoordsByIP } = require('./iss_promised');
+// const { fetchISSFlyOverTimes } = require('./iss_promised');
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = function(passTimes) {
+  for (const p of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(p.risetime);
+    const duration = p.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+
+
+
+
+
+
+
